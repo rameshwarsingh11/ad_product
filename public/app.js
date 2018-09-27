aa964de9d896db9e2d41a81effccfd4e6eaea033
Rameshwar Singh <home-v3@Rameshwar-Singh.local>
2018-09-27 12:05:30 +0530

    adding user setup, helper middleware etc

--- public/app.js
+++ public/app.js
@@ -1,11 +1,28 @@
+// =======================
+// get the packages we need ============
+// =======================
 const express = require('express')
 const app = express()
 app.use(express.json());
 const path = require('path')
 const fetch = require('node-fetch')
-const PORT = process.env.PORT || 3003
+
 var mysql = require('mysql');
 var product = require('./product.js');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User = require('./models/users'); // get our mongoose model
+
+// =======================
+// configuration =========
+// =======================
+const PORT = process.env.PORT || 3006; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -25,7 +42,71 @@ con.connect(function(err) {
   });
 });
 
-app.get('/review/:product_id', (req, res) => {
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function(req, res) {
+  res.send('Hello! The API is running at http://localhost:' + PORT);
+});
+
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var rsingh = new User({
+    name: 'R Singh',
+    password: 'password',
+    admin: true
+  });
+
+  // save the sample user
+  rsingh.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({
+      success: true
+    });
+  });
+});
+
+// API ROUTES -------------------
+
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({
+    message: 'Welcome to the product reviews API!'
+  });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
+
+app.get('/api/review/:product_id', (req, res) => {
   var id = req.params.product_id;
   console.log('Inside review get call :::');
   con.query('select * from product_reviews where product_id =?', [id], function(err, rows) {
@@ -36,7 +117,7 @@ app.get('/review/:product_id', (req, res) => {
   });
 });
 
-app.post('/review/', (req, res) => {
+app.post('/api/review/', (req, res) => {
   var input = JSON.parse(JSON.stringify(req.body));
   console.log('Inside add review post call :::');
   var data = {
@@ -70,7 +151,7 @@ app.put('/review/:product_id', (req, res) => {
   });
 });
 
-app.delete('/review/:product_id', (req, res) => {
+app.delete('/api/review/:product_id', (req, res) => {
   var id = req.params.product_id;
   console.log('Inside review delete call :::');
   con.query('delete from product_reviews where product_id =?', [id], function(err, rows) {
@@ -90,10 +171,10 @@ function get(url) {
   })
 }
 
-app.get('/product/:product_id', (req, res) => {
+app.get('/api/product/:product_id', (req, res) => {
   Promise.all([
       get(`https://www.adidas.co.uk/api/products/${req.params.product_id}`),
-      get(`http://localhost:${PORT}/review/${req.params.product_id}`),
+      get(`http://localhost:${PORT}/api/review/${req.params.product_id}`),
     ]).then(([product, {
         rows
       }]) =>
---
+++ public/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'supersecret',
+  'database': 'mongodb://rsingh:secret123@ds115283.mlab.com:15283/dummy-ad'
+};
