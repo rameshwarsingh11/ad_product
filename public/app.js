a2a6bbf13467f8381cc42a5f7d8ef865dc25c25b
Rameshwar Singh <home-v3@Rameshwar-Singh.local>
2018-09-27 12:48:47 +0530

    added authentication middleware

--- public/app.js
+++ public/app.js
@@ -20,7 +20,7 @@ var User = require('./models/users'); // get our mongoose model
 // =======================
 // configuration =========
 // =======================
-const PORT = process.env.PORT || 3006; // used to create, sign, and verify tokens
+const PORT = process.env.PORT || 3007; // used to create, sign, and verify tokens
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
@@ -84,18 +84,96 @@ app.get('/setup', function(req, res) {
 // get an instance of the router for api routes
 var apiRoutes = express.Router();
 
-// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
 
-// TODO: route middleware to verify a token
+// route to authenticate a user (POST http://localhost:<PORT>/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
 
-// route to show a random message (GET http://localhost:8080/api/)
-apiRoutes.get('/', function(req, res) {
-  res.json({
-    message: 'Welcome to the product reviews API!'
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({
+        success: false,
+        message: 'Authentication failed. User not found.'
+      });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. Wrong password.'
+        });
+      } else {
+        // if user is found and password is correct
+        // then create a token with only our given payload
+        // we don't want to pass in the entire user since that has the password and this may be a security threat
+        const payload = {
+          admin: user.admin
+        };
+        var token = jwt.sign(payload, app.get('superSecret'), {
+          expiresIn: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+
   });
 });
 
-// route to return all users (GET http://localhost:8080/api/users)
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+});
+
+// route to show a random message (GET http://localhost:<PORT>/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({
+    message: 'Welcome to the AD product reviews API!'
+  });
+});
+
+// route to return all users (GET http://localhost:<PORT>/api/users)
 apiRoutes.get('/users', function(req, res) {
   User.find({}, function(err, users) {
     res.json(users);
@@ -104,7 +182,6 @@ apiRoutes.get('/users', function(req, res) {
 
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
-
 
 app.get('/api/review/:product_id', (req, res) => {
   var id = req.params.product_id;
@@ -134,7 +211,7 @@ app.post('/api/review/', (req, res) => {
   });
 });
 
-app.put('/review/:product_id', (req, res) => {
+app.put('/api/review/:product_id', (req, res) => {
   var input = JSON.parse(JSON.stringify(req.body));
   console.log('Inside add review edit call :::');
   var id = req.params.product_id;
